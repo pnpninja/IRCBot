7d598bb898fd9196faea2aff858b6cde2bc976bd
Prateek Narendra <prateeksapple@gmail.com>
2014-09-24 22:07:04 +0530

    Initial Commit

--- IRCBot.py
+++ IRCBot.py
@@ -1,34 +1,28 @@
+#To do - 
+#1 - Handle Private chats and Channel Chats
+#2 - More text processing of message to give a personal touch to messages
+
 import socket
-       
-server = "irc.freenode.net" # Server
-channel = "#lug-nitk" # Channel
-botnickname = "PNPBot" # Your bots nick
 
+server = "irc.freenode.net" #server name
+channel = "#lug-nitk" #channel name
+botnick = "PNPBot" #nickname
 
-def ping(): 
-  sock.send("PONG :pingis\n")  
-
-def sendmsg(chan , msg):
-  sock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
-
-def join_channel(chan): 
+def join_channel(chan):
   sock.send("JOIN "+ chan +"\n")
 
-def initialhello(): 
-  sock.send("PRIVMSG "+ chan +" :Hello!\n")
-                  
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((server, 6667)) # Here we connect to the server using the port 6667
-sock.send("USER "+ botnickname +" "+ botnickname +" "+ botnickname+"This is pnpninja\'s bot\n") # user authentication
-sock.send("NICK "+ botnickname +"\n") # here we actually assign the nick to the bot
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a socket
+sock.connect((server, 6667)) #connect to the IRC server
+sock.send("USER "+ botnick +" "+ botnick +" "+ botnick+"This is pnpninja\'s bot\n") #set user details
+sock.send("NICK "+ botnick +"\n") #set nickname
 
-join_channel(channel) # Join the channel using the functions we previously defined
+join_channel(channel)
 
-while 1: # Be careful with these! it might send you to an infinite loop
-   msg = ircsock.recv(2048) # receive data from the server
-   msg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
-   print(ircmsg) # Here we print what's coming from the server
-   if msg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
-      sock.send("PRIVMSG "+ channel +" :Hello! I am PNPNinja's Bot\n")
-   if msg.find("PING :") != -1: # if the server pings us then we've got to respond!
+while 1:
+   msg = sock.recv(2048)
+   msg = msg.strip('\n\r')
+   print(msg)
+   if msg.find(":Hello "+ botnick) != -1:
+      sock.send("PRIVMSG "+ channel +" :Hello!\n")
+   if msg.find("PING :") != -1:
       sock.send("PONG :pingis\n") 
---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple IRC ChatBot to respond for me while I'm away from my computer
+
+**WE ARE GONNA ADD A LOT MORE TEXT PROCESSING POWER TO THIS - WATCH THIS AND STAR THIS GUYS!!**
